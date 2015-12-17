c1eae3e3aa2332bbbcdd67706d943c8339f1ae74
Sean Nelson <audiohacked@gmail.com>
2015-12-17 00:44:02 -0700

    wtf

--- bootstrap.js
+++ bootstrap.js
@@ -1,20 +1,54 @@
-const Cu = Components.utils;
+'use strict';
+let {classes: Cc, interfaces: Ci, utils: Cu} = Components;
 
 Cu.import('resource://gre/modules/Services.jsm');
+Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 
 const extensionLink = 'chrome://tabutilsbookmarks/',
 contentLink = extensionLink + 'content/',
 uiModuleLink = contentLink + 'ui.jsm',
 helperModuleLink = contentLink + 'helper.jsm',
+timestampModuleLink = contentLink + 'timestamp.js',
 bookmarksModuleLink = contentLink + 'tabutilsbookmarks.js';
 
 
 function startup(data,reason) {
-	Cu.import(helperModuleLink)
-	Cu.import(bookmarksModuleLink);
+	Cu.import(helperModuleLink);
+	Cu.import(timestampModuleLink);
+	// Cu.import(bookmarksModuleLink);
+
+	forEachOpenWindow(loadIntoWindow);
+	Services.wm.addListener(WindowListener);
+}
+
+function shutdown(data,reason) {
+	if (reason == APP_SHUTDOWN)
+		return;
+
+	forEachOpenWindow(unloadFromWindow);
+	Services.wm.removeListener(WindowListener);
+
+	Cu.unload(bookmarksModuleLink);
+	Cu.unload(helperModuleLink);
 	
-	tabutilsbookmarks.init();
-	
+	Services.obs.notifyObservers(null, "chrome-flush-caches", null);
+}
+
+function install(data,reason) {}
+
+function uninstall(data,reason) {}
+
+function loadIntoWindow(window) {
+	/* call/move your UI construction function here */
+	Helper.hookCode("PlacesCommandHook.bookmarkCurrentPages",
+		["this.uniqueCurrentPages", (function() {
+			!gPrivateBrowsingUI.privateBrowsingEnabled && TU_getPref("extensions.tabutilsbookmarks.bookmarkAllWithHistory", true) ?
+				Array.map(gBrowser.allTabs, function(aTab) [aTab.linkedBrowser.currentURI, [{name: 'bookmarkProperties/tabState', value: tabutilsbookmarks._ss.getTabState(aTab)}]]) :
+				Array.map(gBrowser.allTabs, function(aTab) aTab.linkedBrowser.currentURI);
+		}).toString().replace(/^.*{|}$/g, "")],
+			["pages.length > 1", "true"]
+	);
+
 	Helper.hookCode("BookmarkPropertiesPanel._determineItemInfo",
 		['this._strings.getString("bookmarkAllTabsDefault")', "dialogInfo.URIList[0] instanceof Ci.nsIURI ? $& : new Date().toMilitaryString()"]
 	);
@@ -23,23 +57,67 @@ function startup(data,reason) {
 		[/(?=.*_getURITitleFromHistory.*)/, "var annos; if (!(uri instanceof Ci.nsIURI)) [uri, annos] = uri;"],
 		[/title(?=\))/, "$&, null, annos"]
 	);
-	
 }
 
-function shutdown(data,reason) {
-	if (reason == APP_SHUTDOWN)
-		return;
-	
-	Cu.unload(bookmarksModuleLink);
-	Cu.unload(helperModuleLink);
-	
-	Services.obs.notifyObservers(null, "chrome-flush-caches", null);
+function unloadFromWindow(window) {
+/* call/move your UI tear down function here */
 }
 
-function install(data) {
-	
+function forEachOpenWindow(todo)  // Apply a function to all open browser windows
+{
+	var windows = Services.wm.getEnumerator("navigator:browser");
+	while (windows.hasMoreElements())
+		todo(windows.getNext().QueryInterface(Components.interfaces.nsIDOMWindow));
 }
 
-function uninstall() {
-	
+var WindowListener = 
+{
+	onOpenWindow: function(xulWindow) {
+		var window = xulWindow.QueryInterface(Components.interfaces.nsIInterfaceRequestor)
+								.getInterface(Components.interfaces.nsIDOMWindow);
+		function onWindowLoad()
+		{
+			window.removeEventListener("load",onWindowLoad);
+			if (window.document.documentElement.getAttribute("windowtype") == "navigator:browser")
+				var item = window.document.getElementById("context_bookmarkTab");
+				if (item) {
+					item.setAttribute("oncommand", "gBrowser.bookmarkTab(gBrowser.mContextTabs);");
+					item.setAttribute("multiselected", "any");
+				}
+				loadIntoWindow(window);
+		}
+		window.addEventListener("load",onWindowLoad);
+	},
+	onCloseWindow: function(xulWindow) { },
+	onWindowTitleChange: function(xulWindow, newTitle) { }
+};
+
+if (!Date.prototype.toMilitaryString) {
+	(function() {
+		function pre_pad(number, length) {
+			var str = "" + number;
+			while (str.length < length) {
+				str = '0'+str;
+			}
+			return str;
+		}
+		function post_pad(number, length) {
+			var str = "" + number;
+			while (str.length < length) {
+				str = str+'0';
+			}
+			return str;
+		}
+		Date.prototype.toMilitaryString = function() {
+			var offset = this.getTimezoneOffset();
+			var hour_offset = pre_pad(offset/60, 2);
+			return this.getUTCFullYear() +
+				'-' + pre_pad(this.getUTCMonth() + 1, 2) +
+				'-' + pre_pad(this.getUTCDate(), 2) +
+				' ' + pre_pad(this.getHours(), 2) +
+				':' + pre_pad(this.getUTCMinutes(), 2) +
+				':' + pre_pad(this.getUTCSeconds(), 2) +
+				' GMT' + (offset<0 ? '+' : '-') + post_pad(hour_offset, 4);
+		};
+	}());
 }
--- chrome/content/tabutilsbookmarks.js
+++ chrome/content/tabutilsbookmarks.js
@@ -2,24 +2,26 @@
 
 Components.utils.import("resource://gre/modules/Services.jsm");
 Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
+Components.utils.import("chrome://tabutilsbookmarks/content/helper.jsm");
 
 var tabutilsbookmarks = {
 	init: function() {
-		// this._bookmarkTabs();
-		this._multiTabHandler();
+		Helper.hookCode("PlacesCommandHook.bookmarkCurrentPages",
+			["this.uniqueCurrentPages", (function() {
+				!gPrivateBrowsingUI.privateBrowsingEnabled && TU_getPref("extensions.tabutilsbookmarks.bookmarkAllWithHistory", true) ?
+					Array.map(gBrowser.allTabs, function(aTab) [aTab.linkedBrowser.currentURI, [{name: 'bookmarkProperties/tabState', value: tabutilsbookmarks._ss.getTabState(aTab)}]]) :
+					Array.map(gBrowser.allTabs, function(aTab) aTab.linkedBrowser.currentURI);
+			}).toString().replace(/^.*{|}$/g, "")],
+				["pages.length > 1", "true"]
+		);
 
-		// window.addEventListener("load", this, false);
-		// window.addEventListener("unload", this, false);
-		//
-		// this.fxOnOS = Services.appinfo.OS; //WINNT, Linux or Darwin
-		// this.fxVersion = parseFloat(Services.appinfo.version);
-		// document.documentElement.setAttribute("OS", this.FxOnOS);
-		// document.documentElement.setAttribute("v4", true);
-		// document.documentElement.setAttribute("v6", true);
-		// document.documentElement.setAttribute("v14", true);
-		// document.documentElement.setAttribute("v17", true);
-		// document.documentElement.setAttribute("v29", this.fxVersion >= 29.0);
-		// document.documentElement.setAttribute("v31", this.fxVersion >= 31.0);
+		var item = window.document.getElementById("context_bookmarkTab");
+		if (item) {
+			item.setAttribute("oncommand", "gBrowser.bookmarkTab(gBrowser.mContextTabs);");
+			item.setAttribute("multiselected", "any");
+		}
+		// this._bookmarkTabs();
+		// this._multiTabHandler();
 	},
 
 	onload: function() {
@@ -33,7 +35,6 @@ var tabutilsbookmarks = {
 
 	onunload: function() {
 		this._eventListeners.forEach(function(args) document.removeEventListener.apply(args[0], Array.slice(args, 1)));
-		// this._tagsFolderObserver.uninit();
 	},
 
 	handleEvent: function(event) {
@@ -47,16 +48,16 @@ var tabutilsbookmarks = {
 };
 // window.addEventListener("DOMContentLoaded", tabutilsbookmarks, false);
 
-[
-  ["@mozilla.org/browser/sessionstore;1", "nsISessionStore", "_ss", tabutilsbookmarks], // Bug 898732 [Fx26]
-  ["@mozilla.org/docshell/urifixup;1", "nsIURIFixup"], // Bug 802026 [Fx20]
-  ["@mozilla.org/places/colorAnalyzer;1", "mozIColorAnalyzer"],
-  ["@mozilla.org/uuid-generator;1", "nsIUUIDGenerator"]
-].forEach(function([aContract, aInterface, aName, aObject])
-  XPCOMUtils.defineLazyServiceGetter(aObject || Services, aName || aInterface, aContract, aInterface)
-);
+// [
+// 	["@mozilla.org/browser/sessionstore;1", "nsISessionStore", "_ss", tabutilsbookmarks], // Bug 898732 [Fx26]
+// ].forEach(function([aContract, aInterface, aName, aObject])
+// 	XPCOMUtils.defineLazyServiceGetter(aObject || Services, aName || aInterface, aContract, aInterface)
+// );
 
-// Bookmark tabs with history
+XPCOMUtils.defineLazyServiceGetter(tabutilsbookmarks || Services, "_ss" || "nsISessionStore", "@mozilla.org/browser/sessionstore;1", "nsISessionStore");
+
+
+// // Bookmark tabs with history
 // tabutilsbookmarks._bookmarkTabs = function() {
 //   gBrowser.bookmarkTab = function(aTabs) {
 //     if (!("length" in aTabs))
@@ -85,22 +86,21 @@ var tabutilsbookmarks = {
 //   );
 // };
 
-tabutilsbookmarks._multiTabHandler = function() {
-
-  // Select Multiple Tabs
-  // gBrowser.__defineGetter__("allTabs", function() {
-  //   return this.visibleTabs.slice(this._numPinnedTabs);
-  // });
-
-	[
-		["context_bookmarkTab", "gBrowser.bookmarkTab(gBrowser.mContextTabs);"],
-	].forEach(function([aId, aCommand]) {
-		var item = document.getElementById(aId);
-		if (item) {
-			item.setAttribute("oncommand", aCommand);
-			item.setAttribute("multiselected", "any");
-		}
-	});
-}
+// tabutilsbookmarks._multiTabHandler = function() {
+// 	// Select Multiple Tabs
+// 	gBrowser.__defineGetter__("allTabs", function() {
+// 		return this.visibleTabs.slice(this._numPinnedTabs);
+// 	});
+//
+// 	[
+// 		["context_bookmarkTab", "gBrowser.bookmarkTab(gBrowser.mContextTabs);"],
+// 	].forEach(function([aId, aCommand]) {
+// 		var item = document.getElementById(aId);
+// 		if (item) {
+// 			item.setAttribute("oncommand", aCommand);
+// 			item.setAttribute("multiselected", "any");
+// 		}
+// 	});
+// }
 
 var EXPORTED_SYMBOLS = ['tabutilsbookmarks'];
